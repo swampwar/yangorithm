ab1fdfd0551758248f77270a77adedb3479eadbe
user <user@121.133.118.113>
2018-03-06 00:24:18 +0900

    fucking final turn

--- yangorithm/src/Codility/Challenge/Manganum_FinalTurn.java
+++ yangorithm/src/Codility/Challenge/Manganum_FinalTurn.java
@@ -20,12 +20,120 @@ public class Manganum_FinalTurn {
 		System.out.println(new Manganum_FinalTurn().solution(X2, Y2, T));
 
 	}
+	
+	public int getMaxValue(int[] array) {
+		int[] tempX = Arrays.copyOfRange(array, 0, array.length);
+		Arrays.sort(tempX);
+		return tempX[tempX.length-1];
+	}
+	
+	public int getMinValue(int[] array) {
+		int[] tempX = Arrays.copyOfRange(array, 0, array.length);
+		Arrays.sort(tempX);
+		return tempX[0];
+	}
+	
+	public int isPoint(int[] X, int[] Y, int x, int y){
+		for(int idx=0; idx<X.length; idx++){
+			if(X[idx] == x && Y[idx] == y) return idx;
+		}
+		return -1;
+	}
+	
+	public int getScore(char c) {
+		if(c == 'p') {
+			return 1;
+		}else if(c == 'q') {
+			return 10;
+		}
+		return 0;
+	}
+	
+	
+	public int[] getRightPoint(int[] X, int[] Y, int[] queen, int score, String T) {
+		int max_x = getMaxValue(X);
+		int max_y = getMaxValue(Y);
+		
+		System.out.println("max_x : " + max_x);
+		System.out.println("max_y : " + max_y);
+
+		int moved_queen_x = queen[0]+1;
+		int moved_queen_y = queen[1]+1;
+		
+		int[] right_point = {0, 0, score};
+		
+		while(!(moved_queen_x > max_x || moved_queen_y > max_y)) {
+			int pointIdx = isPoint(X, Y, moved_queen_x, moved_queen_y);
+			
+			if(pointIdx != -1) {
+				if(isPoint(X, Y, moved_queen_x+1, moved_queen_y+1) == -1) {
+					right_point[0] = moved_queen_x;
+					right_point[1] = moved_queen_y;
+					right_point[2] = score + getScore(T.charAt(pointIdx));
+					
+					System.out.println("get right point");
+					System.out.println(moved_queen_x + "," + moved_queen_y + "::::" +right_point[2]);
+					
+				}else {
+					System.out.println("오른쪽은 탈락!");
+				}
+				
+				break;
+			}
+			
+			moved_queen_x++;
+			moved_queen_y++;
+		}
+		
+		return right_point;
+	}
+	
+	public int[] getLeftPoint(int[] X, int[] Y, int[] queen, int score, String T) {
+		int min_x = getMinValue(X);
+		int max_y = getMaxValue(Y);
+		
+		System.out.println("min_x : " + min_x);
+		System.out.println("max_y : " + max_y);
+
+		int moved_queen_x = queen[0]-1;
+		int moved_queen_y = queen[1]+1;
+		
+		int[] left_point = {0, 0, score};
+		
+		while(!(moved_queen_x < min_x || moved_queen_y > max_y)) {
+			int pointIdx = isPoint(X, Y, moved_queen_x, moved_queen_y);
+			if(pointIdx != -1) {
+				if(isPoint(X, Y, moved_queen_x-1, moved_queen_y+1) == -1) {
+					left_point[0] = moved_queen_x;
+					left_point[1] = moved_queen_y;
+					left_point[2] = score + getScore(T.charAt(pointIdx));
+					
+					System.out.println("get left point");
+					System.out.println(moved_queen_x + "," + moved_queen_y + "::::" +left_point[2]);
+				}else {
+					System.out.println("왼쪽은 탈락!");
+				}
+				break;
+			}
+			
+			moved_queen_x--;
+			moved_queen_y++;
+		}
+		
+		return left_point;
+	}
+	
 
 	public int solution(int[] X, int[] Y, String T){
 		int queenIdx = T.indexOf('X');
+		int[] queen = {X[queenIdx],Y[queenIdx]};
+		int score = 0;
+
+		int[] right_point = getRightPoint(X, Y, queen, score, T);
+		int[] left_point = getLeftPoint(X, Y, queen, score, T);
 		
 		// 1. get access point 
-		int[] queen = {X[queenIdx],Y[queenIdx]};
+		/*
 		int min_distX_right = 10;
 		int min_distX_left = 10;
 		Integer[] r_point = new Integer[2];
@@ -64,7 +172,7 @@ public class Manganum_FinalTurn {
 		for(int i=0; i<access_point_list.size(); i++){
 			System.out.println(Arrays.toString(access_point_list.get(i)));
 		}
-		
+		*/
 		// 2. sum point
 		
 		
@@ -73,11 +181,8 @@ public class Manganum_FinalTurn {
 		return 0;
 	}
 	
-	public boolean isPoint(int[] X, int[] Y, int x, int y){
-		for(int idx=0; idx<X.length; idx++){
-			if(X[idx] == x && Y[idx] == y) return true;
-		}
-		return false;
-	}
+
+	
+	
 
 }
