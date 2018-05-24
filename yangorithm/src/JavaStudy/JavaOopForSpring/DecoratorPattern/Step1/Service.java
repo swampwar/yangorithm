3371c371df9891fce31f25fc7cf947b9cf584cf2
swampwar <dkdfl88@gmail.com>
2018-05-24 17:30:13 +0900

    ㅎㅅ

---
+++ yangorithm/src/JavaStudy/JavaOopForSpring/DecoratorPattern/Step1/Service.java
@@ -0,0 +1,10 @@
+package JavaStudy.JavaOopForSpring.DecoratorPattern.Step1;
+
+public class Service implements IService{
+
+	@Override
+	public String runSomething() {
+		return "서비스 실행결과";
+	}
+
+}
---
+++ yangorithm/src/JavaStudy/JavaOopForSpring/TemplateMethodPattern/SubClassA.java
@@ -0,0 +1,10 @@
+package JavaStudy.JavaOopForSpring.TemplateMethodPattern;
+
+public class SubClassA extends TemplateMethodClass {
+
+	@Override
+	public void abstractMethod() {
+		System.out.println("abstract method override in subClassA");
+	}
+
+}
