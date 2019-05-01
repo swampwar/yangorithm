2fc95eb60515b4628845fe5a04d390501b3e535c
swampwar <dkdfl88@gmail.com>
2019-05-01 22:46:56 +0900

    카테코리 풀었당!

--- src/main/java/TCT/JavaA_2018/_05Category.java
+++ src/main/java/TCT/JavaA_2018/_05Category.java
@@ -1,62 +1,154 @@
 package TCT.JavaA_2018;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _05Category {
+
+    List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
 
         _05Category obj = new _05Category();
+
         String[][] input1 = new String[][]{
                 {"M", "B"}, {"M", "C"}, {"M", "K"},
                 {"B", "E"}, {"C", "F"}, {"C", "G"}, {"C", "H"}, {"K", "I"}, {"K", "J"},
                 {"E", "D"}, {"F", "L"}, {"F", "A"}, {"H", "N"}, {"H", "O"}, {"J", "P"}, {"J", "Q"}
         };
 
-        System.out.println(input1.length);
-        obj.getList(input1);
+
+        obj.run(input1, "F", "N", "J");
+
+        System.out.println("-------------------------------------------------------------------");
+
+
+        String[][] input2 = new String[][]{
+                {"Z", "B"}, {"Z", "W"}, {"Z", "V"},
+                {"B", "E"}, {"W", "F"}, {"W", "G"}, {"V", "H"}, {"V", "I"}, {"V", "J"},
+                {"E", "K"}, {"F", "L"}, {"G", "M"}, {"G", "N"}, {"H", "O"}, {"I", "P"}, {"J", "Q"}
+        };
+
+        obj.run(input2, "I", "O", "G");
+
+    }
+
+    public void run(String[][] input, String a, String b, String c){
+        list = getList(input);
+        System.out.println(list);
+
+        String rsltStr = getTopCategory(input, Arrays.asList(a, b));
+        System.out.println(rsltStr);
+
+        int rsltint = getNumberOfSubcategories(input, c);
+        System.out.println(rsltint);
 
     }
 
     public String getTopCategory(String[][] inputData, List<String> categories){
+        List<String> list2 = new ArrayList<>();
 
-        return null;
-    }
-
-    public int getNumberOfSubcategories(String[][] inputData, String categoryStr){
-        return 0;
-    }
-
-    List<String> getList(String[][] input1){
-        List<String> list = new ArrayList<>();
-        String startStr = input1[0][0];
-
-        for(int i=0; i<input1.length; i++){
-            String nowStr = input1[i][0];
-            String nowStr2 = input1[i][1];
-            System.out.println("현재 nowStr: " + i + ", "+ nowStr);
-
-            if(nowStr.equals(startStr)){
-                System.out.println("nowStr == startStr : " + startStr);
-                list.add(startStr + nowStr2);
-            }else{
-                System.out.println("nowStr != startStr : " + nowStr);
-                for(int j=0; j < list.size(); j++){
-                    String s = list.get(j);
-                    String lastStr = s.substring(s.length()-1);
-                    if(lastStr.equals(nowStr)){
-                        System.out.println("lastStr == nowStr : " + s);
-                        list.remove(j);
-                        list.add(j, s+nowStr2);
-                    }
+        for(int i=0; i<list.size(); i++){
+            String str = list.get(i);
+            if(str.indexOf(categories.get(0)) != -1){
+                if(!list2.contains(str)){
+                    list2.add(str);
                 }
-
             }
-            System.out.println(list);
+
+            if(str.indexOf(categories.get(1)) != -1){
+                if(!list2.contains(str)){
+                    list2.add(str);
+                }
+            }
 
         }
 
-        System.out.println(list);
+        System.out.println(list2);
+
+
+        String checkStr = list2.get(0).substring(0,1);
+        String rsltStr = "";
+        boolean flag = true;
+        for(int i=0; ;i++){
+            for(int j=0; j<list2.size(); j++){
+                if(list2.get(j).startsWith(checkStr)){
+                }else{
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(!flag) break;
+            rsltStr = checkStr;
+            checkStr = list2.get(0).substring(0, i+2);
+        }
+
+
+
+        return rsltStr.substring(rsltStr.length()-1, rsltStr.length());
+    }
+
+    public int getNumberOfSubcategories(String[][] inputData, String categoryStr){
+        List<String> list2 = new ArrayList<>();
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).indexOf(categoryStr) != -1){
+                categoryStr = list.get(i).substring(list.get(i).indexOf(categoryStr)-1, list.get(i).indexOf(categoryStr));
+                break;
+            }
+        }
+
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).indexOf(categoryStr) != -1){
+                if(!list2.contains(list.get(i))){
+                    list2.add(list.get(i));
+                }
+            }
+        }
+
+        System.out.println("list2 : " + list2);
+        Set<Character> set = new HashSet<Character>();
+        for(int i=0; i<list2.size(); i++){
+            String str = list2.get(i).substring(list2.get(i).indexOf(categoryStr)+1);
+            char[] chars = str.toCharArray();
+            for(char c : chars){
+                set.add(c);
+            }
+        }
+
+
+        System.out.println(set);
+        return set.size();
+    }
+
+    List<String> getList(String[][] input){
+        List<String> list = new ArrayList<>();
+        String startStr = input[0][0];
+
+        for(int i=0; i<input.length; i++){
+            if(startStr.equals(input[i][0])){
+                list.add(input[i][0] + input[i][1]);
+            }else{
+
+                for(int j=0; j<list.size(); j++){
+                    if(list.get(j).indexOf(input[i][0]) != -1){ // 마지막에 포함된다면
+                        list.add(list.get(j) + input[i][1]);
+
+                        if(i==input.length-1){
+                            list.remove(j);
+                        }else{
+                            if(!input[i+1][0].equals(input[i][0])){
+                                list.remove(j);
+                            }
+                        }
+                        break;
+                    }
+
+                }
+
+            }
+        }
 
         return list;
     }
